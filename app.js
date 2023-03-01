1031bc8f1bac48718aa1ef1d5d50cc62164e2298
DaliborStolarski <32590244+DaliborStolarski@users.noreply.github.com>
2023-03-01 10:52:23 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,74 @@
+// requiring modules
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+const mailchimp = require("@mailchimp/mailchimp_marketing");
+
+// init constant app
+const app = express();
+
+// public folder with IMG and CSS
+app.use(express.static("public"));
+
+// body-parser use
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Sending signup.html to browser ASAP a request is made on localhost:3000
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/signup.html");
+});
+
+//Setting up MailChimp
+mailchimp.setConfig({
+  // apiKey and server
+  apiKey: "4f0d237de7cabab14e770a2624ff79e5-us13",
+  server: "us13",
+});
+
+//As soon as the sign in button is pressed execute this (based on HTML data)
+app.post("/", function (req, res) {
+  var firstName = req.body.fName;
+  var lastName = req.body.lName;
+  var email = req.body.email;
+  const listId = "4ca79d5234";
+
+  //Creating an object with the users data
+  const subscribingUser = {
+    firstName: firstName,
+    lastName: lastName,
+    email: email,
+  };
+
+  //Uploading the data to the server
+  async function run() {
+    const response = await mailchimp.lists.addListMember(listId, {
+      email_address: subscribingUser.email,
+      status: "subscribed",
+      merge_fields: {
+        FNAME: subscribingUser.firstName,
+        LNAME: subscribingUser.lastName,
+      },
+    });
+
+    //If all goes well logging the contact's id
+    res.sendFile(__dirname + "/success.html");
+    console.log(
+      `Successfully added contact as an audience member. The contact's id is ${response.id}.`
+    );
+  }
+  //Running the function and catching the errors (if any)
+  // catch statement is executed when there is an error, if anything goes wrong the code in the catch code is executed. In the catch block we're sending back the failure page. This means if anything goes wrong send the faliure page
+  run().catch((e) => res.sendFile(__dirname + "/failure.html"));
+  console.log(`Error occurred!`);
+});
+
+// port 3000 listener
+app.listen(process.env.PORT || 3000, function () {
+  console.log("server is running on port 3000, Newsletter-Signup project");
+});
+
+// API key
+// 4f0d237de7cabab14e770a2624ff79e5-us13
+
+// List ID
+// 4ca79d5234
